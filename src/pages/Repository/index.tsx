45778ae4650ee7f849ffc5f3631bbf57053b1831
Leandro Rocha <prog.leandro@gmail.com>
2020-05-03 15:06:29 -0300

    Estilizandon o detalhe

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { Header, RepositoryInfo, Issues } from './styles';
+
+import logoImg from '../../assets/logo.svg';
 
 interface RepositoryParams {
   repository: string;
@@ -8,10 +12,47 @@ const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
   return (
-    <h1>
-      Repository:
-      {params.repository}
-    </h1>
+    <>
+      <Header>
+        <img src={logoImg} alt="github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img src="https://avatars3.githubusercontent.com/u/4483666?s=460&u=80e553025b11af70c0764ff71b01bdc9e16007f0&v=4" />
+          <div>
+            <strong>RocketSeat</strong>
+            <p>Testete</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>123</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>123</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>123</strong>
+            <span>Issues Abertos</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+      <Issues>
+        <Link to="ss">
+          <div>
+            <strong>dad</strong>
+            <p>dasd</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
   );
 };
 
